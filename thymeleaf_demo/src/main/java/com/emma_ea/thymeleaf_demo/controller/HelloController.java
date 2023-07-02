8bd2974acc19341764327b2b107ab5d942402e41
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2023-07-02 01:21:56 +0000

    add user attrib to template

--- thymeleaf_demo/src/main/java/com/emma_ea/thymeleaf_demo/controller/HelloController.java
+++ thymeleaf_demo/src/main/java/com/emma_ea/thymeleaf_demo/controller/HelloController.java
@@ -1,13 +1,26 @@
 package com.emma_ea.thymeleaf_demo.controller;
 
+import com.emma_ea.thymeleaf_demo.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HelloController {
+
+    @RequestMapping("users")
+    public String users(Model model) {
+        List<User> users = new ArrayList<>();
+        users.add(new User(1, "John", 34));
+        users.add(new User(2, "Will", 67));
+        users.add(new User(3, "Sam", 20));
+        model.addAttribute("users", users);
+        return "hello2";
+    }
 
     @RequestMapping("hello")
     public String demo(Model model) {
