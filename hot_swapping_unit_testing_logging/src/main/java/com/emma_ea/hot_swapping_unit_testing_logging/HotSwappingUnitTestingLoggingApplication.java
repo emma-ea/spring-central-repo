bfdd40fd608dc55bef35fe57ed37ca1ec89c2642
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2023-07-02 18:27:42 +0000

    initial commit

---
+++ hot_swapping_unit_testing_logging/src/main/java/com/emma_ea/hot_swapping_unit_testing_logging/HotSwappingUnitTestingLoggingApplication.java
@@ -0,0 +1,13 @@
+package com.emma_ea.hot_swapping_unit_testing_logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotSwappingUnitTestingLoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotSwappingUnitTestingLoggingApplication.class, args);
+	}
+
+}
---
+++ hot_swapping_unit_testing_logging/src/test/java/com/emma_ea/hot_swapping_unit_testing_logging/HotSwappingUnitTestingLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emma_ea.hot_swapping_unit_testing_logging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotSwappingUnitTestingLoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
