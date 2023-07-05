ba17a30b14d0ecf1f80d5dca7a18637d22467f70
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2023-07-05 21:20:05 +0000

    setup message and message wrapper

---
+++ websockets_and_stomp/src/main/java/com/emma_ea/websockets_and_stomp/model/UserResponse.java
@@ -0,0 +1,15 @@
+package com.emma_ea.websockets_and_stomp.model;
+
+public class UserResponse {
+
+    private String content;
+
+    public UserResponse() {}
+
+    public UserResponse(String content) { this.content = content; }
+
+    public String getContent() { return this.content; }
+
+    public void setContent(String content) { this.content = content; }
+
+}
