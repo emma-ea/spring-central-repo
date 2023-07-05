2e98d32c45db154d0ac24bd51fba8c994c7ff3fc
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2023-07-05 21:44:02 +0000

    messaging scheduler

---
+++ websockets_and_stomp/src/main/java/com/emma_ea/websockets_and_stomp/config/UserSchedulerConfig.java
@@ -0,0 +1,22 @@
+package com.emma_ea.websockets_and_stomp.config;
+
+import com.emma_ea.websockets_and_stomp.model.UserResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@Configuration
+@EnableScheduling
+public class UserSchedulerConfig {
+
+    @Autowired
+    SimpMessagingTemplate template;
+
+    @Scheduled(fixedDelay = 5000)
+    public void scheduleMessage() {
+        template.convertAndSend("/topic/user", new UserResponse("scheduler"));
+    }
+
+}
