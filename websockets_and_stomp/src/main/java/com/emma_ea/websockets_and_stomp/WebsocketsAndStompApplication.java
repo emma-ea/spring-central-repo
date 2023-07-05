f7902131b5dc0b872b158701642c6acafacf7fe7
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2023-07-05 20:59:45 +0000

    initial commit

---
+++ websockets_and_stomp/src/main/java/com/emma_ea/websockets_and_stomp/WebsocketsAndStompApplication.java
@@ -0,0 +1,13 @@
+package com.emma_ea.websockets_and_stomp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketsAndStompApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketsAndStompApplication.class, args);
+	}
+
+}
