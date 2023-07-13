711a28f855b906f8180460c23948b9d1ab070a9c
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2023-07-13 22:57:09 +0000

    impl queries for graphql operations

--- dog_graphql/src/main/java/com/emma_ea/dog_graphql/repository/DogRepository.java
+++ dog_graphql/src/main/java/com/emma_ea/dog_graphql/repository/DogRepository.java
@@ -6,11 +6,17 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface DogRepository extends CrudRepository<Dog, Long> {
 
     @Query("SELECT d.breed FROM Dog d")
     List<String> retrieveBreeds();
+
+    @Query("SELECT d.name FROM Dog d")
+    List<String> retrieveNames();
+
+    Boolean deleteByBreed(String breed);
 
 }
