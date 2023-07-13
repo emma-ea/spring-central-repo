477395314867bbeee9396a0c9bb1c2c2d0a15074
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2023-07-13 02:30:32 +0000

    setup graphql deps/configs

---
+++ dog_graphql/src/main/java/com/emma_ea/dog_graphql/DogGraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.emma_ea.dog_graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogGraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogGraphqlApplication.class, args);
+	}
+
+}
---
+++ dog_graphql/src/test/java/com/emma_ea/dog_graphql/DogGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emma_ea.dog_graphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogGraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
