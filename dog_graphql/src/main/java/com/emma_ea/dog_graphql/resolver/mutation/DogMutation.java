af57b2fb8e711c7f75722c104a646336a4d17cb2
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2023-07-16 21:07:56 +0000

    rm graphql kickstarter dep

--- dog_graphql/src/main/java/com/emma_ea/dog_graphql/resolver/mutation/DogMutation.java
+++ dog_graphql/src/main/java/com/emma_ea/dog_graphql/resolver/mutation/DogMutation.java
@@ -1,46 +1,46 @@
-package com.emma_ea.dog_graphql.resolver.mutation;
-
-import com.emma_ea.dog_graphql.entity.Dog;
-import com.emma_ea.dog_graphql.exception.BreedNotFoundException;
-import com.emma_ea.dog_graphql.exception.FindDogBreedException;
-import com.emma_ea.dog_graphql.repository.DogRepository;
-import graphql.kickstart.tools.GraphQLMutationResolver;
-import org.springframework.stereotype.Component;
-
-import java.util.List;
-import java.util.Optional;
-
-@Component
-public class DogMutation implements GraphQLMutationResolver {
-
-    private final DogRepository repository;
-
-    public DogMutation(DogRepository repository) {
-        this.repository = repository;
-    }
-
-    Boolean deleteDogBreed(String breed) {
-        List<Dog> dogList = (List<Dog>) repository.findAll();
-        boolean deleted = false;
-        for (Dog d : dogList) {
-            if (d.getBreed().equals(breed)) {
-                repository.delete(d);
-                deleted = true;
-            }
-        }
-        if (!deleted)
-            throw new BreedNotFoundException("Could not delete dog. No matching breed found", breed);
-        return deleted;
-    }
-
-    Dog updateDogName(String newName, Long id) {
-        Optional<Dog> dog = repository.findById(id);
-        if (dog.isPresent()) {
-            Dog newDog = dog.get();
-            newDog.setName(newName);
-            repository.save(newDog);
-            return newDog;
-        }
-        throw new FindDogBreedException("Could not update dog. No matching id.", id);
-    }
-}
+//package com.emma_ea.dog_graphql.resolver.mutation;
+//
+//import com.emma_ea.dog_graphql.entity.Dog;
+//import com.emma_ea.dog_graphql.exception.BreedNotFoundException;
+//import com.emma_ea.dog_graphql.exception.FindDogBreedException;
+//import com.emma_ea.dog_graphql.repository.DogRepository;
+//import graphql.kickstart.tools.GraphQLMutationResolver;
+//import org.springframework.stereotype.Component;
+//
+//import java.util.List;
+//import java.util.Optional;
+//
+//@Component
+//public class DogMutation implements GraphQLMutationResolver {
+//
+//    private final DogRepository repository;
+//
+//    public DogMutation(DogRepository repository) {
+//        this.repository = repository;
+//    }
+//
+//    public Boolean deleteDogBreed(String breed) {
+//        List<Dog> dogList = (List<Dog>) repository.findAll();
+//        boolean deleted = false;
+//        for (Dog d : dogList) {
+//            if (d.getBreed().equals(breed)) {
+//                repository.delete(d);
+//                deleted = true;
+//            }
+//        }
+//        if (!deleted)
+//            throw new BreedNotFoundException("Could not delete dog. No matching breed found", breed);
+//        return deleted;
+//    }
+//
+//    public Dog updateDogName(String newName, Long id) {
+//        Optional<Dog> dog = repository.findById(id);
+//        if (dog.isPresent()) {
+//            Dog newDog = dog.get();
+//            newDog.setName(newName);
+//            repository.save(newDog);
+//            return newDog;
+//        }
+//        throw new FindDogBreedException("Could not update dog. No matching id.", id);
+//    }
+//}
--- dog_graphql/src/main/java/com/emma_ea/dog_graphql/resolver/query/DogQueryResolver.java
+++ dog_graphql/src/main/java/com/emma_ea/dog_graphql/resolver/query/DogQueryResolver.java
@@ -1,38 +1,38 @@
-package com.emma_ea.dog_graphql.resolver.query;
-
-import com.emma_ea.dog_graphql.entity.Dog;
-import com.emma_ea.dog_graphql.exception.FindDogBreedException;
-import com.emma_ea.dog_graphql.repository.DogRepository;
-import com.emma_ea.dog_graphql.service.DogService;
-import graphql.kickstart.tools.GraphQLQueryResolver;
-import org.springframework.stereotype.Component;
-
-import java.util.List;
-import java.util.Optional;
-
-@Component
-public class DogQueryResolver implements GraphQLQueryResolver {
-
-    private final DogRepository repository;
-
-    public DogQueryResolver(DogRepository repository) {
-        this.repository = repository;
-    }
-
-    List<Dog> findDogBreeds() {
-        return (List<Dog>) repository.findAll();
-    }
-
-    String findDogBreedById(long id) {
-        Optional<Dog> dog = repository.findById(id);
-        if (dog.isPresent()) {
-            return dog.get().getBreed();
-        }
-        throw new FindDogBreedException("No dog found with id", id);
-    }
-
-    List<String> findAllDogNames() {
-        return repository.retrieveNames();
-    }
-
-}
+//package com.emma_ea.dog_graphql.resolver.query;
+//
+//import com.emma_ea.dog_graphql.entity.Dog;
+//import com.emma_ea.dog_graphql.exception.FindDogBreedException;
+//import com.emma_ea.dog_graphql.repository.DogRepository;
+//import com.emma_ea.dog_graphql.service.DogService;
+//import graphql.kickstart.tools.GraphQLQueryResolver;
+//import org.springframework.stereotype.Component;
+//
+//import java.util.List;
+//import java.util.Optional;
+//
+//@Component
+//public class DogQueryResolver implements GraphQLQueryResolver {
+//
+//    private final DogRepository repository;
+//
+//    public DogQueryResolver(DogRepository repository) {
+//        this.repository = repository;
+//    }
+//
+//    public List<Dog> findDogBreeds() {
+//        return (List<Dog>) repository.findAll();
+//    }
+//
+//    public String findDogBreedById(long id) {
+//        Optional<Dog> dog = repository.findById(id);
+//        if (dog.isPresent()) {
+//            return dog.get().getBreed();
+//        }
+//        throw new FindDogBreedException("No dog found with id", id);
+//    }
+//
+//    public List<String> findAllDogNames() {
+//        return repository.retrieveNames();
+//    }
+//
+//}
