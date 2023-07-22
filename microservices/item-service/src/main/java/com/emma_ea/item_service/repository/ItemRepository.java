84a0d6e85fa8cd6e63873986a62436c22290ed93
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2023-07-22 21:37:02 +0000

    add config deps for registering client on eureka

--- microservices/item-service/src/main/java/com/emma_ea/item_service/repository/ItemRepository.java
+++ microservices/item-service/src/main/java/com/emma_ea/item_service/repository/ItemRepository.java
@@ -1,6 +1,6 @@
 package com.emma_ea.item_service.repository;
 
 import com.emma_ea.item_service.entity.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface ItemRepository extends JpaRepository<Item, Long> { }
+public interface ItemRepository extends CrudRepository<Item, Long> { }
