67b8c9d21886c2b43b25edb6832f9734fa933f92
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2023-07-26 02:02:58 +0000

    initial commit

---
+++ dog-secure/src/main/java/com/emma_ea/dog_secure/DogSecureApplication.java
@@ -0,0 +1,13 @@
+package com.emma_ea.dog_secure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogSecureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogSecureApplication.class, args);
+	}
+
+}
---
+++ dog-secure/src/test/java/com/emma_ea/dog_secure/DogSecureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emma_ea.dog_secure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogSecureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
