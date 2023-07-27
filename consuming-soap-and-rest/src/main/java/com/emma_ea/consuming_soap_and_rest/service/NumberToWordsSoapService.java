b1ceb925ceb3e96973f616321029aaf1cb8457cb
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2023-07-27 04:25:55 +0000

    add wsdl. generate entities

---
+++ consuming-soap-and-rest/src/main/java/com/emma_ea/consuming_soap_and_rest/service/NumberToWordsSoapService.java
@@ -0,0 +1,9 @@
+package com.emma_ea.consuming_soap_and_rest.service;
+
+import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
+
+public class NumberToWordsSoapService extends WebServiceGatewaySupport {
+
+
+
+}
