8e462dee98ca0f26068a481df75bb52bb0bfad76
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2023-07-28 21:17:07 +0000

    wip - soap client config

--- consuming-soap-and-rest/src/main/java/com/emma_ea/consuming_soap_and_rest/service/NumberToWordsSoapService.java
+++ consuming-soap-and-rest/src/main/java/com/emma_ea/consuming_soap_and_rest/service/NumberToWordsSoapService.java
@@ -1,9 +1,22 @@
 package com.emma_ea.consuming_soap_and_rest.service;
 
+import com.dataaccess.webservicesserver.NumberToWords;
+import com.dataaccess.webservicesserver.NumberToWordsResponse;
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
+
+import java.math.BigInteger;
 
 public class NumberToWordsSoapService extends WebServiceGatewaySupport {
 
+    public NumberToWordsResponse getWords(String number) {
+        String uri = "https://www.dataaccess.com/webservicesserver/numberconversion.wso";
 
+        NumberToWords numberRequest = new NumberToWords();
+        numberRequest.setUbiNum(new BigInteger(number));
+
+        NumberToWordsResponse numberResponse =
+                (NumberToWordsResponse) getWebServiceTemplate().marshalSendAndReceive(uri, numberRequest);
+        return numberResponse;
+    }
 
 }
