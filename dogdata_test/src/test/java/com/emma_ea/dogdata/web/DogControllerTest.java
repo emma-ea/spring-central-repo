7a30fcdd43e5e50d5b77cef9b4f8b19ce31579dd
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2023-07-30 23:58:58 +0000

    test retrieve all

--- dogdata_test/src/test/java/com/emma_ea/dogdata/web/DogControllerTest.java
+++ dogdata_test/src/test/java/com/emma_ea/dogdata/web/DogControllerTest.java
@@ -5,10 +5,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(DogController.class)
 class DogControllerTest {
@@ -21,7 +27,11 @@ class DogControllerTest {
 
     @Test
     public void getAllDogs() throws Exception {
+        mockMvc.perform(get("/api/v1/dogs/all"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
 
+        verify(service, times(1)).retrieveAll();
     }
 
 }
