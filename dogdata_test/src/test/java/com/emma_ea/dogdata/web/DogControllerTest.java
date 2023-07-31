77bd9a9e9908d376e6d268f00d09fd40cb7d378e
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2023-07-31 00:39:51 +0000

    test retrieve by id

--- dogdata_test/src/test/java/com/emma_ea/dogdata/web/DogControllerTest.java
+++ dogdata_test/src/test/java/com/emma_ea/dogdata/web/DogControllerTest.java
@@ -1,5 +1,6 @@
 package com.emma_ea.dogdata.web;
 
+import com.emma_ea.dogdata.entity.Dog;
 import com.emma_ea.dogdata.service.DogService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,8 +11,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -29,9 +29,22 @@ class DogControllerTest {
     public void getAllDogs() throws Exception {
         mockMvc.perform(get("/api/v1/dogs/all"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json("[]"));
 
         verify(service, times(1)).retrieveAll();
+    }
+
+    @Test
+    public void retrieveById() throws Exception {
+        Dog mockDog = new Dog();
+        mockDog.setId(1L);
+        when(service.retrieveById(1L)).thenReturn(java.util.Optional.of(mockDog));
+
+        mockMvc.perform(get("/api/v1/dogs/id").queryParam("id", "1"))
+                .andExpect(status().isOk());
+
+        verify(service, times(1)).retrieveById(1L);
     }
 
 }
